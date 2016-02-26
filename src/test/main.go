ccae0860b3dd8b919354b1294e5a1d95a62ac707
developer-yjh <519279044@qq.com>
2016-02-26 22:57:44 +0800

    测试1111111

--- src/test/main.go
+++ src/test/main.go
@@ -7,6 +7,10 @@ import (
 func main() {
 	msg := "我们都是中国人呀, 中国龙!"
 	PrintMessage(msg)
+
+	msg = "外国人的干活"
+	PrintMessage(msg)
+
 	fmt.Println("this is test main func")
 
 	fmt.Println("this is main.go")
